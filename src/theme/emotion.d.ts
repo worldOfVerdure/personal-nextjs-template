2aaff38448cb1a0242de83b47c412c4ad12933b2
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-09-18 16:23:57 -0500

    Patch: ESLint and Emotion theme typing cleanup

--- src/theme/emotion.d.ts
+++ src/theme/emotion.d.ts
@@ -1,6 +1,6 @@
 import '@emotion/react';
 import { Theme as MuiTheme } from '@mui/material/styles';
-
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module '@emotion/react' {
   export interface Theme extends MuiTheme {}
 }
